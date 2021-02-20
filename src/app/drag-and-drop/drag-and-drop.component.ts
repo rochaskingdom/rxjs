2a2db6bf5d300f98201a0fc6a2add24faa0eede1
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-02-20 18:55:12 -0300

    Drag-and-drop com observables Parte 2

--- src/app/drag-and-drop/drag-and-drop.component.ts
+++ src/app/drag-and-drop/drag-and-drop.component.ts
@@ -1,13 +1,15 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-drag-and-drop',
   templateUrl: './drag-and-drop.component.html',
   styleUrls: ['./drag-and-drop.component.css']
 })
-export class DragAndDropComponent implements OnInit {
+export class DragAndDropComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('myRect') myRect: ElementRef;
+  @ViewChild('myrect') myrect: ElementRef;
 
   top = 40;
   left = 40;
@@ -16,6 +18,32 @@ export class DragAndDropComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const mousedown = fromEvent(this.myrect.nativeElement, 'mousedown');
+    const mousemove = fromEvent(document, 'mousemove');
+    const mouseup = fromEvent(document, 'mouseup');
+
+    mousedown.subscribe((ed: MouseEvent) => {
+      // console.log(ed);
+      let x = ed.pageX;
+      let y = ed.pageY;
+
+      mousemove
+        .pipe(
+          takeUntil(mouseup)
+        )
+        .subscribe((em: MouseEvent) => {
+          // console.log(em);
+          const offSetX = x - em.pageX;
+          const offSetY = y - em.pageY;
+          this.top -= offSetY;
+          this.left -= offSetX;
+          x = em.pageX;
+          y = em.pageY;
+        });
+    });
   }
 
 }
